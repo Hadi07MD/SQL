606dc1845778b08286b81ef2eb2c862152e06bba
Hadi07MD <hadiiisoli94@gmail.com>
2022-03-10 15:27:32 +0100

    SELECT

--- test.sql
+++ test.sql
@@ -1,4 +1,9 @@
 
-create database `student`;
+CREATE DATABASE `student`;
 
 USE `student`; CREATE TABLE students (id int PRIMARY KEY, Nom Varchar (250));
+
+INSERT INTO 'students' ('id','nom') VALUES ('1','HADI').
+
+SELECT  nom, note FROM eleve INNER JOIN note ON eleve.idf = note.idf;
+
